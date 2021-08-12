312edcdd8e77087bfc9150f6aa99e4c39a5ace3c
jamesallen0351 <james.allen0351@gmail.com>
2021-08-12 17:00:05 -0500

    add classification exercises in progress

---
+++ acquire.py
@@ -0,0 +1,4 @@
+# Make a function named get_titanic_data that returns the titanic data from the codeup data science database as a pandas data frame 
+# #Obtain your data from the Codeup Data Science Database.
+
+get_titanic_data = 
